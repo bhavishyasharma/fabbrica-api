9929f4f0e8fbd74e0b21792dabe7cfce835d8ee8
Bhavishya Sharma <bhavishyasharma@live.com>
2020-04-14 02:44:12 +0530

    Update Company

--- fabbrica/company/mutations.py
+++ fabbrica/company/mutations.py
@@ -2,6 +2,7 @@ import graphene
 from flask_jwt_extended import ( jwt_required )
 from .type import CompanyType
 from .model import CompanyModel
+from core.user.model import UserModel
 
 class AddCompanyMutation(graphene.Mutation):
     company = graphene.Field(CompanyType)
@@ -18,3 +19,22 @@ class AddCompanyMutation(graphene.Mutation):
         )
         company.save()
         return AddCompanyMutation(company=company)
+
+class UpdateCompanyMutation(graphene.Mutation):
+    company = graphene.Field(CompanyType)
+
+    class Arguments:
+        companyId = graphene.String(required=True)
+        name = graphene.String()
+        users = graphene.List(graphene.String)
+
+    @jwt_required
+    def mutate(self, info, companyId, name=None, users=None):
+        company = CompanyModel.objects(id=companyId).get()
+        if name is not None:
+            company.name = name
+        if users is not None:
+            usersList = UserModel.objects(id__in=users).all()
+            company.users = usersList
+        company.save()
+        return UpdateCompanyMutation(company=company)
--- fabbrica/company/schema.py
+++ fabbrica/company/schema.py
@@ -4,11 +4,12 @@ from core.common.type import Filter, Pagination
 from core.common.helpers import filter_qs, pagination_qs, sortings_qs
 from .type import CompanyType
 from .model import CompanyModel
-from .mutations import AddCompanyMutation
+from .mutations import AddCompanyMutation, UpdateCompanyMutation
 
 class Query(graphene.ObjectType):
     companies = graphene.List(CompanyType, filters=graphene.List(Filter, required=False), 
         pagination=Pagination(required=False), sortings = graphene.List(graphene.String, required=False))
+    company = graphene.Field(CompanyType, companyId=graphene.String(required=True))
     
     @jwt_required
     def resolve_companies(self, info, filters=None, pagination=None, sortings=None):
@@ -18,7 +19,12 @@ class Query(graphene.ObjectType):
         qs = sortings_qs(qs, sortings)
         return list(CompanyModel.objects().all())
 
+    def resolve_company(self, info, companyId=None):
+        company = CompanyModel.objects(id=companyId).get()
+        return company
+
 class Mutation(graphene.ObjectType):
     add_company = AddCompanyMutation.Field()
+    update_company = UpdateCompanyMutation.Field()
 
 schema = graphene.Schema(query=Query, types=[CompanyType], mutation=Mutation)
