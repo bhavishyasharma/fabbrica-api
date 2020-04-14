a7e24ead3271af5ee8b88f1623e46a57b5cdfbd9
Bhavishya Sharma <bhavishyasharma@live.com>
2020-04-14 12:02:28 +0530

    close #2

--- fabbrica/mould/mutations.py
+++ fabbrica/mould/mutations.py
@@ -2,6 +2,7 @@ import graphene
 from flask_jwt_extended import ( jwt_required )
 from .type import MouldType
 from .model import MouldModel
+from ..part.model import PartModel
 
 class AddMouldMutation(graphene.Mutation):
     mould = graphene.Field(MouldType)
@@ -22,3 +23,28 @@ class AddMouldMutation(graphene.Mutation):
         )
         mould.save()
         return AddMouldMutation(mould=mould)
+
+class UpdateMouldMutation(graphene.Mutation):
+    mould = graphene.Field(MouldType)
+
+    class Arguments:
+        mouldId = graphene.String(required=True)
+        name = graphene.String()
+        part = graphene.String()
+        cavity = graphene.Int()
+        runnerWeight = graphene.Decimal()
+
+    @jwt_required
+    def mutate(self, info, mouldId, name=None, part=None, cavity=None, runnerWeight=None):
+        mould = MouldModel.objects(id=mouldId).get()
+        if name is not None:
+            mould.name = name
+        if part is not None:
+            partDocument = PartModel.objects(id=part).get()
+            mould.part = partDocument
+        if cavity is not None:
+            mould.cavity = cavity
+        if runnerWeight is not None:
+            mould.runnerWeight = runnerWeight
+        mould.save()
+        return UpdateMouldMutation(mould=mould)
--- fabbrica/mould/schema.py
+++ fabbrica/mould/schema.py
@@ -4,11 +4,12 @@ from core.common.type import Filter, Pagination
 from core.common.helpers import filter_qs, pagination_qs, sortings_qs
 from .type import MouldType
 from .model import MouldModel
-from .mutations import AddMouldMutation
+from .mutations import AddMouldMutation, UpdateMouldMutation
 
 class Query(graphene.ObjectType):
     moulds = graphene.List(MouldType, filters=graphene.List(Filter, required=False), 
         pagination=Pagination(required=False), sortings = graphene.List(graphene.String, required=False))
+    mould = graphene.Field(MouldType, mouldId=graphene.String(required=True))
     
     @jwt_required
     def resolve_moulds(self, info, filters=None, pagination=None, sortings=None):
@@ -17,8 +18,13 @@ class Query(graphene.ObjectType):
         qs = pagination_qs(qs, pagination)
         qs = sortings_qs(qs, sortings)
         return list(MouldModel.objects().all())
+    
+    def resolve_mould(self, info, mouldId=None):
+        mould = MouldModel.objects(id=mouldId).get()
+        return mould
 
 class Mutation(graphene.ObjectType):
     add_mould = AddMouldMutation.Field()
+    update_mould = UpdateMouldMutation.Field()
 
 schema = graphene.Schema(query=Query, types=[MouldType], mutation=Mutation)
